5381169140ed3081792440f35a63cd1c651b1731
Deniz KALFA <dkalfa@gmail.com>
2014-01-06 20:18:18 +0000

    @thyms improved details form page

--- app/assets/coffeescripts/app/application.coffee
+++ app/assets/coffeescripts/app/application.coffee
@@ -4,6 +4,14 @@ controllers =
     $scope.model.step = 1
     $scope.model.customer = new Customer()
 
+    # Defaults
+    $scope.model.customer.serviceRequest = {}
+    $scope.model.customer.serviceRequest.numberOfCopies = 1
+    $scope.model.customer.serviceRequest.numberOfApostilles = 1
+
+    $scope.model.customer.card = {}
+    $scope.model.customer.card.type = 'visa'
+
     $scope.goToStep = (step)->
       $scope.model.step = step
     $scope.previousStep = ->
@@ -14,69 +22,45 @@ controllers =
     $scope.saveStepAdditionalInfo = ->
       Customer.create($scope.model.customer)
 
-    $scope.model.days = [
-      name: '1', value: 1
+    $scope.model.numberOfCopies = ({name: num, value: num} for num in [1..10])
+    $scope.model.numberOfApostilles = ({name: num, value: num} for num in [1..10])
+    $scope.model.yearsExpiry = ({name: num, value: num} for num in [2014..2020])
+    $scope.model.coutries = []
+    $scope.model.cardTypes = [
+      name: 'Visa', value: 'visa'
     ,
-      name: '2', value: 2
+      name: 'Visa Debit', value: 'visaDebit'
     ,
-      name: '3', value: 3
+      name: 'Master', value: 'master'
     ,
-      name: '4', value: 4
-    ,
-      name: '5', value: 5
-    ,
-      name: '6', value: 6
-    ,
-      name: '7', value: 7
-    ,
-      name: '8', value: 8
-    ,
-      name: '9', value: 9
-    ,
-      name: '10', value: 10
-    ,
-      name: '11', value: 11
-    ,
-      name: '12', value: 12
-    ,
-      name: '13', value: 13
-    ,
-      name: '14', value: 14
-    ,
-      name: '15', value: 15
-    ,
-      name: '16', value: 16
-    ,
-      name: '17', value: 17
-    ,
-      name: '18', value: 18
-    ,
-      name: '19', value: 19
-    ,
-      name: '20', value: 20
-    ,
-      name: '21', value: 21
-    ,
-      name: '22', value: 22
-    ,
-      name: '23', value: 23
-    ,
-      name: '24', value: 24
-    ,
-      name: '25', value: 25
-    ,
-      name: '26', value: 26
-    ,
-      name: '27', value: 27
-    ,
-      name: '28', value: 28
-    ,
-      name: '29', value: 29
-    ,
-      name: '30', value: 30
-    ,
-      name: '31', value: 31
+      name: 'Maestro', value: 'maestro'
     ]
+    $scope.model.months = [
+      name: 'January', value: 1
+    ,
+      name: 'February', value: 2
+    ,
+      name: 'March', value: 3
+    ,
+      name: 'April', value: 4
+    ,
+      name: 'May', value: 5
+    ,
+      name: 'June', value: 6
+    ,
+      name: 'July', value: 7
+    ,
+      name: 'August', value: 8
+    ,
+      name: 'September', value: 9
+    ,
+      name: 'October', value: 10
+    ,
+      name: 'November', value: 11
+    ,
+      name: 'December', value: 12
+    ]
+    $scope.model.days = ({name: num, value: num} for num in [1..31])
   ]
 
 factories =
--- public/javascripts/app/application.js
+++ public/javascripts/app/application.js
@@ -4,9 +4,15 @@
   controllers = {
     CertificateDetailsController: [
       "$scope", "Customer", function($scope, Customer) {
+        var num;
         $scope.model = {};
         $scope.model.step = 1;
         $scope.model.customer = new Customer();
+        $scope.model.customer.serviceRequest = {};
+        $scope.model.customer.serviceRequest.numberOfCopies = 1;
+        $scope.model.customer.serviceRequest.numberOfApostilles = 1;
+        $scope.model.customer.card = {};
+        $scope.model.customer.card.type = 'visa';
         $scope.goToStep = function(step) {
           return $scope.model.step = step;
         };
@@ -19,102 +25,105 @@
         $scope.saveStepAdditionalInfo = function() {
           return Customer.create($scope.model.customer);
         };
-        return $scope.model.days = [
+        $scope.model.numberOfCopies = (function() {
+          var _i, _results;
+          _results = [];
+          for (num = _i = 1; _i <= 10; num = ++_i) {
+            _results.push({
+              name: num,
+              value: num
+            });
+          }
+          return _results;
+        })();
+        $scope.model.numberOfApostilles = (function() {
+          var _i, _results;
+          _results = [];
+          for (num = _i = 1; _i <= 10; num = ++_i) {
+            _results.push({
+              name: num,
+              value: num
+            });
+          }
+          return _results;
+        })();
+        $scope.model.yearsExpiry = (function() {
+          var _i, _results;
+          _results = [];
+          for (num = _i = 2014; _i <= 2020; num = ++_i) {
+            _results.push({
+              name: num,
+              value: num
+            });
+          }
+          return _results;
+        })();
+        $scope.model.coutries = [];
+        $scope.model.cardTypes = [
           {
-            name: '1',
-            value: 1
+            name: 'Visa',
+            value: 'visa'
           }, {
-            name: '2',
-            value: 2
+            name: 'Visa Debit',
+            value: 'visaDebit'
           }, {
-            name: '3',
-            value: 3
+            name: 'Master',
+            value: 'master'
           }, {
-            name: '4',
-            value: 4
-          }, {
-            name: '5',
-            value: 5
-          }, {
-            name: '6',
-            value: 6
-          }, {
-            name: '7',
-            value: 7
-          }, {
-            name: '8',
-            value: 8
-          }, {
-            name: '9',
-            value: 9
-          }, {
-            name: '10',
-            value: 10
-          }, {
-            name: '11',
-            value: 11
-          }, {
-            name: '12',
-            value: 12
-          }, {
-            name: '13',
-            value: 13
-          }, {
-            name: '14',
-            value: 14
-          }, {
-            name: '15',
-            value: 15
-          }, {
-            name: '16',
-            value: 16
-          }, {
-            name: '17',
-            value: 17
-          }, {
-            name: '18',
-            value: 18
-          }, {
-            name: '19',
-            value: 19
-          }, {
-            name: '20',
-            value: 20
-          }, {
-            name: '21',
-            value: 21
-          }, {
-            name: '22',
-            value: 22
-          }, {
-            name: '23',
-            value: 23
-          }, {
-            name: '24',
-            value: 24
-          }, {
-            name: '25',
-            value: 25
-          }, {
-            name: '26',
-            value: 26
-          }, {
-            name: '27',
-            value: 27
-          }, {
-            name: '28',
-            value: 28
-          }, {
-            name: '29',
-            value: 29
-          }, {
-            name: '30',
-            value: 30
-          }, {
-            name: '31',
-            value: 31
+            name: 'Maestro',
+            value: 'maestro'
           }
         ];
+        $scope.model.months = [
+          {
+            name: 'January',
+            value: 1
+          }, {
+            name: 'February',
+            value: 2
+          }, {
+            name: 'March',
+            value: 3
+          }, {
+            name: 'April',
+            value: 4
+          }, {
+            name: 'May',
+            value: 5
+          }, {
+            name: 'June',
+            value: 6
+          }, {
+            name: 'July',
+            value: 7
+          }, {
+            name: 'August',
+            value: 8
+          }, {
+            name: 'September',
+            value: 9
+          }, {
+            name: 'October',
+            value: 10
+          }, {
+            name: 'November',
+            value: 11
+          }, {
+            name: 'December',
+            value: 12
+          }
+        ];
+        return $scope.model.days = (function() {
+          var _i, _results;
+          _results = [];
+          for (num = _i = 1; _i <= 31; num = ++_i) {
+            _results.push({
+              name: num,
+              value: num
+            });
+          }
+          return _results;
+        })();
       }
     ]
   };
