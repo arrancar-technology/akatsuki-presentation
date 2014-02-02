c47a9007bf909bdac8b3658c3bf2e2042e9b94a3
Deniz KALFA <dkalfa@gmail.com>
2014-02-02 18:04:58 +0000

    @thyms updated db config for environments.

--- config/environments/config.js
+++ config/environments/config.js
@@ -5,14 +5,29 @@ var config = {
     }
   },
   func01: {
+    database: {
+      url:  process.env.MONGODB_URL
+    }
   },
   qa01: {
+    database: {
+      url:  process.env.MONGODB_URL
+    }
   },
   demo01: {
+    database: {
+      url:  process.env.MONGODB_URL
+    }
   },
   stage01: {
+    database: {
+      url:  process.env.MONGODB_URL
+    }
   },
   prod01: {
+    database: {
+      url:  process.env.MONGODB_URL
+    }
   }
 };
 
