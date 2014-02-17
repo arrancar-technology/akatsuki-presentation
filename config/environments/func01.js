aaf9c91b28dc3fb54b64d8e73c4431561a5e8f5b
Deniz KALFA <dkalfa@gmail.com>
2014-02-17 19:58:09 +0000

    @thyms added creation of admin user on functional environment.

--- config/environments/func01.js
+++ config/environments/func01.js
@@ -7,5 +7,19 @@ module.exports = function (compound) {
     app.disable('assets timestamps');
     app.use(require('express').errorHandler());
     app.settings.quiet = true;
+
+    // Create users
+    var db = require('./../../app/service/DbService')(compound);
+    db.collection('users').drop(function() {
+      console.log('>> dropped the users collection...');
+
+      var users = [{username: 'admin', email: 'admin@akatsuki.com', password: 'password'}];
+      users.forEach(function(user) {
+        db.collection('users').save(user, function(err, result) {
+          if (err) console.log('>> err: ', err);
+          if (result) console.log('>> created user: ', result.username);
+        });
+      });
+    });
   });
 };
