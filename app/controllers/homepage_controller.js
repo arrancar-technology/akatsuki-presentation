8156697db606ac20388f9fdb8a9b9734464ee097
Deniz KALFA <dkalfa@gmail.com>
2014-04-05 20:04:08 +0100

    @thyms added feature switch for homepage to display different content.

--- app/controllers/homepage_controller.js
+++ app/controllers/homepage_controller.js
@@ -1,3 +1,7 @@
 action('show', function(){
-  render({title: 'Simply Certificate'});
+  // TODO: [DK] remove this after go live
+  var environment = process.env.NODE_ENV || 'development';
+  var layout = environment === 'prod01' && req.query.debug !== 'true' ? 'show_prod' : 'show';
+
+  render(layout, {title: 'Simply Certificate'});
 });
